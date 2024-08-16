c328cf5080889569e0aa13d5d7c07e50e4424be1
Денис Касперович <kasperovich.deonis@gmail.com>
2024-08-16 16:15:32 +0300

    hw 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,17 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            if(action.payload === 'up'){
+                return [...state.sort((a, b) => a.name.localeCompare(b.name))]
+            }
+            return [...state.sort((a, b) => b.name.localeCompare(a.name))] // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            const fullAge = state.filter(u => u.age > action.payload) // need to fix
+            return fullAge.sort((a, b) => a.age - b.age)
         }
         default:
             return state
