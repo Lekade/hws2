51f0213da3619819cef3d183aa24d4a43593fd1e
Денис Касперович <kasperovich.deonis@gmail.com>
2024-08-26 19:15:24 +0300

    styling fixes

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,25 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+const initialPeople: UserType[] = [
+    {_id: 0, name: 'Кот', age: 3},
+    {_id: 1, name: 'Александр', age: 66},
+    {_id: 2, name: 'Коля', age: 16},
+    {_id: 3, name: 'Виктор', age: 44},
+    {_id: 4, name: 'Дмитрий', age: 40},
+    {_id: 5, name: 'Ирина', age: 55},
+]
+
+export const homeWorkReducer = (state: UserType[] = initialPeople, action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
             if(action.payload === 'up'){
-                return [...state.sort((a, b) => a.name.localeCompare(b.name))]
+                return [...state].sort((a, b) => a.name.localeCompare(b.name))
             }
-            return [...state.sort((a, b) => b.name.localeCompare(a.name))] // need to fix
+            return [...state].sort((a, b) => b.name.localeCompare(a.name)) // need to fix
         }
         case 'check': {
-            const fullAge = state.filter(u => u.age > action.payload) // need to fix
-            return fullAge.sort((a, b) => a.age - b.age)
+            return state.filter(u => u.age >= action.payload) // need to fix
         }
         default:
             return state
