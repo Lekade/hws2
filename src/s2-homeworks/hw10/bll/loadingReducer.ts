a2f95700a0f6d6281c9d084445ba343761710681
Денис Касперович <kasperovich.deonis@gmail.com>
2024-09-04 17:11:22 +0300

    hw 10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,21 +2,27 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+
+export const loadingReducer = (state:InitialStateType = initState, action: ActionType): InitialStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING':
+            return {...state, isLoading: action.isLoading}
         default:
             return state
     }
 }
 
-type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
     type: 'CHANGE_LOADING',
     isLoading,
 })
+
+type InitialStateType = typeof initState
+
+type LoadingActionType = {
+    type: 'CHANGE_LOADING'
+    isLoading: boolean
+}
+type ActionType = LoadingActionType
