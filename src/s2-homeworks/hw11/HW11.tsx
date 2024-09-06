bb725e046f5a7f40e4fb5e29fa0e7cc3935af2db
Денис Касперович <kasperovich.deonis@gmail.com>
2024-09-06 15:58:34 +0300

    hw 11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
+import {restoreState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
@@ -13,10 +13,20 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 function HW11() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+    const [value2, setValue2] = useState(restoreState<number[]>('hw11-value2', [value1, 100]))
 
-    const change = (event: any, value: any) => {
+    const change = (event: Event, value: number | number[]) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+        if(typeof value === 'number'){
+            setValue1(value)
+            setValue2(prevState =>  [value, prevState[1]])
+        }else {
+            if(value2[0] !== value[0]){
+                setValue1(value[0])
+            }
+            setValue2(prevState =>  [value[0], value[1]])
+        }
+
     }
 
     return (
@@ -24,23 +34,27 @@ function HW11() {
             <div className={s2.hwTitle}>Homework #11</div>
 
             <div className={s2.hw}>
-                <div className={s.container}>
-                    <div className={s.wrapper}>
-                        <span id={'hw11-value'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-single-slider'}
-                            // сделать так чтоб value1 изменялось // пишет студент
-
-                        />
-                    </div>
-                    <div className={s.wrapper}>
-                        <span id={'hw11-value-1'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-double-slider'}
-                            // сделать так чтоб value1/2 изменялось // пишет студент
-
-                        />
-                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+                <div className={s2.wrapper}>
+                    <div className={s.container}>
+                        <div className={s.wrapper}>
+                            <span id={'hw11-value'} className={s.number}>{value1}</span>
+                            <SuperRange
+                                id={'hw11-single-slider'}
+                                value={value1}
+                                onChange={change}
+                                // сделать так чтоб value1 изменялось // пишет студент
+                            />
+                        </div>
+                        <div className={s.wrapper}>
+                            <span id={'hw11-value-1'} className={s.number}>{value1}</span>
+                            <SuperRange
+                                id={'hw11-double-slider'}
+                                // сделать так чтоб value1/2 изменялось // пишет студент
+                                value={value2}
+                                onChange={change}
+                            />
+                            <span id={'hw11-value-2'} className={s.number}>{value2[1]}</span>
+                        </div>
                     </div>
                 </div>
             </div>
